72a3ba18ecfc2dbb8608ab103981ad05f74be5a4
Amir <92445549+amiresaye6@users.noreply.github.com>
2023-11-27 15:17:22 +0000

    extra

--- skills.js
+++ skills.js
@@ -1,1 +1,8 @@
-function calculateNumbers(var1, var2)
+function calculateNumbers(var1, var2) {
+    // Add your code here
+    console.log(var1, var2);
+    console.log(var1 + var2);
+
+    return var1 + var2;
+    console.log("This won't be printed");
+}
